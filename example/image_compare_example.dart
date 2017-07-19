3b4e551fa22628605d43039a908c0f345804b747
Matthew Barbour <sanmadjack@gmail.com>
2017-07-19 15:31:09 -0500

    no message

--- example/image_compare_example.dart
+++ example/image_compare_example.dart
@@ -2,7 +2,12 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/angular2.dart';
+import 'package:angular2/platform/browser.dart';
 import 'package:angular_image_compare/image_compare_component.dart';
+
+void main() {
+  bootstrap(CustomComponent);
+}
 
 @Component(
     selector: 'custom-component',
